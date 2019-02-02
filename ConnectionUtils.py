5b8de1ff6141e7814ea32ca53d2ff9f90d86fb26
diachenko <a.diachenko@outlook.com>
2019-02-02 15:45:57 +0200

    trying to do docker

--- ConnectionUtils.py
+++ ConnectionUtils.py
@@ -1,17 +1,40 @@
 import Constants
 import mysql.connector
 
-
 def connection():
     try:
         c = mysql.connector.connect(
             host=Constants.host,
+            port=Constants.port,
             user=Constants.username,
             passwd=Constants.password,
             database=Constants.database
         )
         return c
-    except:
-        print("Could not connect to database!")
+    except mysql.connector.errors.Error as e:
+        print("Connection failed:")
+        print(e)
         exit(1)
 
+    ''' except mysql.connector.errors.DatabaseError as e:
+            try:
+                c1 = mysql.connector.connect(
+                    host=Constants.host,
+                    port=Constants.port,
+                    user=Constants.username,
+                    passwd=Constants.password
+                )
+                print("Instance connection successful")
+                print("Could not find meetwithfriends database!")
+                print("Creating new database")
+                script = open("scripts/init.sql", "r")
+                s = script.read()
+                c1.cursor().execute(s, True)
+                c1.database = 'meetwithfriends'
+                return c1
+            except mysql.connector.errors.Error as e:
+                if e == "Use multi=True when executing multiple statements":
+                    return c1
+                print("Err creating new database:")
+                print(e)
+                exit(1)'''
--- Constants.py
+++ Constants.py
@@ -1,5 +1,6 @@
 username = "username"
 password = "password"
 host = "127.0.0.1"
+port = 3306
 database = "meetwithfriends"
 secret_key = "very_big_secret"
