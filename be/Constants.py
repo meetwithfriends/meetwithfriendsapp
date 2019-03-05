1ae7f6bebfafb0cbdf38ac6c7c4a8ade1e79be0f
diachenko <a.diachenko@outlook.com>
2019-03-05 19:36:44 +0200

    docker worksgit add be/dockerfilegit add be/dockerfile!

--- be/Constants.py
+++ be/Constants.py
@@ -1,6 +1,6 @@
 username = "root"
 password = "password"
-host = "localhost"
-port = 3306
+host = "192.168.99.100" #for windows 192.168.99.100 / for linux 127.0.0.1
+port = 3305
 database = "meetwithfriends"
 secret_key = "very_big_secret"
